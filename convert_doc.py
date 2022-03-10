8387f6eceb704a52c263412544e9dd04748b7566
bluejet80 <know_jarett@yahoo.com>
2022-03-10 06:48:19 -0600

    This is the new study reference website

---
+++ convert_doc.py
@@ -0,0 +1,11 @@
+
+
+
+with open('vi_cmds', 'r+', encoding='utf-8') as file:
+
+    lines = file.readlines()
+    file.seek(0)
+    for line in lines:
+        line = line.rstrip('\n')
+        x = line.split(", ")
+        file.write('<tr><td>' + x[0] + '</td><td>' + x[1] + '</td></tr>\n')
